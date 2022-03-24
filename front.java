bb73ae64727766015077b659e4f5eff5609f26cb
mgmmourya <mgmmourya74@gmail.com>
2022-03-24 08:24:51 +0530

    updatingggg...

--- front.java
+++ front.java
@@ -1,2 +1,3 @@
-Hello....Morning
+Hey...i ma updating here to show merge conflict!!
+Hello....Abey paddai per dhyaan do....master me hai....after commiting on master file:)	
 Thsi is the front page 
